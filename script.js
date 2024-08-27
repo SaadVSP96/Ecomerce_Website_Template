f1473130d1ffd70d4c25db6d93a61db26766c3cf
SaadVSP96 <saadmasood192@gmail.com>
2024-08-27 15:56:45 +0500

    22. Finalizing static version for now, before adding functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// ALL JS CODE:
+
+// Navigation Bar Functionality
+
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
@@ -13,3 +17,37 @@ if (close) {
         nav.classList.remove("active");
     });
 }
+
+// Creating Shop Objects Using JS:
+
+let productData = [
+    "img/products/f1.jpg",
+    "img/products/f2.jpg",
+    "img/products/f3.jpg",
+    "img/products/f4.jpg",
+    "img/products/f5.jpg",
+    "img/products/f6.jpg",
+    "img/products/f7.jpg",
+    "img/products/f8.jpg",
+];
+
+const newProduct = document.createElement("div");
+newProduct.classList.add("pro");
+newProduct.onclick = "window.location.href='single_product.html'";
+// define template literal:
+newProduct.innerHTML = `
+                    <img src=${productData[0]} alt="" />
+                    <div class="desc">
+                        <span>adidas</span>
+                        <h5>Cartoon Astronaut T-Shirts</h5>
+                        <div class="star">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <h4>$78</h4>
+                        <a href="#"><i class="fa fa-shopping-cart cart"></i></a>
+                    </div>
+`;
