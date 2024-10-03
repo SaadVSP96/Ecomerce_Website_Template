ac7edcf830cfcf415c008b90f03ba9aeb7b280e3
SaadVSP96 <saadmasood192@gmail.com>
2024-10-03 18:53:30 +0500

    74. First get all products request successful on postman

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
@@ -15,13 +16,13 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 // routes
-app.use("api/v1/products", productsRouter);
+app.use("/api/v1/products", productsRouter);
 
 // error middleware
 app.use(notFoundMiddleware);
 
 // start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,13 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({});
+    console.log("hit 1");
     res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
     const products = await Product.find({});
+    console.log("hit 2");
     res.status(200).json({ products, nbHits: products.length });
 };
 
