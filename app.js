c13adc502aa9499171366058ae180195f4055298
SaadVSP96 <saadmasood192@gmail.com>
2024-10-03 18:19:04 +0500

    73. Barebones route, router and controller set

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
 const connectDB = require("./db/connect");
-require("dotenv").config();
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not_found");
+
+// middleware
+app.use(express.json());
 
 // serve static files from the public directory
 app.use(express.static("./public"));
-app.use(express.json());
+
+// routes
+app.use("api/v1/products", productsRouter);
+
+// error middleware
+app.use(notFoundMiddleware);
 
 // start the server
 const port = process.env.PORT || 5000;
---
+++ controllers/products.js
@@ -0,0 +1,13 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({});
+    res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+    const products = await Product.find({});
+    res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
